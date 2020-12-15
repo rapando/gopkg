56e3426022003ef8dc4b46092a64fb7cbcfb6b9f
rapando <samsonrapando@gmail.com>
2020-12-15 21:40:53 +0300

    corrected bug in missing Shape

--- gopkg.go
+++ gopkg.go
@@ -1,9 +1,13 @@
 package gopkg
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rapando/gopkg/shape"
+)
 
 // Describe -  Runs all the functions of the interface
-func Describe(s Shape) {
+func Describe(s shape.Shape) {
 	s.Description()
 	fmt.Printf("the area is %v\n", s.Area())
 	fmt.Printf("the volume is %v\n", s.Volume())
